76867bdcb988d482e970e987287b5dfa88f82283
Moran Horovitz Pahima <moran.horovitz@mail.huji.ac.il>
2025-05-06 20:13:50 +0300

    clean

--- supabase/functions/add_contract_backer/index.ts
+++ supabase/functions/add_contract_backer/index.ts
@@ -111,7 +111,7 @@ Deno.serve(async (req) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/add_contract_backer' \
+  curl -i --location --request POST 'http://localhost:54321/functions/v1/add_contract_backer' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
